60afe94e46de42c5f96b2d35ea4194b013a05417
Daniel Rodrigues <danielrodriguescarvalho777@gmail.com>
2024-04-15 18:13:38 -0300

    learning recursive functions

--- Funcoes/recursiveFunctions.js
+++ Funcoes/recursiveFunctions.js
@@ -7,3 +7,21 @@ function dividir(num){
         return num
     }
 }
+
+// dividir(33456)
+
+// Primeira -> Segunda -> Terceira -> Quarta
+
+function factorial(num){
+    console.log(`Número: ${num}`)
+    if(num === 0){
+        return 1
+    } else if (num === 1){
+        return 1
+    } else {
+        console.log(`${num} * ${num -1}!`)
+        return num *factorial(num-1)
+    }
+}
+
+console.log(factorial(100))
