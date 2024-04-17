b81e00bb1c9615e942f4969cb6cc409f94a47a71
Daniel Rodrigues <danielrodriguescarvalho777@gmail.com>
2024-04-17 15:34:24 -0300

    learning highOrderFunctions

--- Funcoes/highOrderFuncitons.js
+++ Funcoes/highOrderFuncitons.js
@@ -1,8 +1,7 @@
 // Função que tem outra como parâmetro
 function calcular(a, b, operacao) {
     console.log("Realizando uma operação")
-    const resultado = operacao(a,b)
-    return resultado
+    return console.log(operacao(a,b))
 }
 
 function somar (x,y){
@@ -11,3 +10,51 @@ function somar (x,y){
 }
 
 calcular(3,5,somar)
+
+calcular(8,4,function (x,y){
+    console.log("Realizando uma subtração")
+    return x - y
+})
+
+function exibirElemento(elemento, indice,array){
+    console.log({
+        elemento,
+        indice,
+        array
+    })
+}
+
+const lista = ["Maçã", "Banana", "Laranja", "Limão"]
+
+for (let i = 0; i < lista.length; i++) {
+    exibirElemento(lista[i], i,lista)
+}
+
+lista.forEach(function  (elemento,indice,array){
+    console.log({
+        elemento,
+        indice,
+        array
+    })
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
